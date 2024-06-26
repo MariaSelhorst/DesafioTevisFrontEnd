386765ae607d37900ed1a795e00e74bdbbae50db
joao <joaoklippel.senai@gmail.com>
2024-06-26 16:17:56 -0300

    Register pages created

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ fetch("http://localhost:8080/test")
         let value = document.getElementsByClassName("message")[0]
         value.innerHTML = text
     })
+
+function openModal(event) {
+    event.preventDefault(); // Prevent form submission
+    document.getElementById('01').style.display = 'block';
+}
+
+function closeModal() {
+    document.getElementById('01').style.display = 'none';
+}
